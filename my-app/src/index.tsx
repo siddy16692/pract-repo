4809234e9975f24f3ea5fcdcd1623851d7689690
sidnhs <siddharth.nair@nhs.net>
2021-08-26 17:49:46 +0100

    Adding amplify auth

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from "@aws-amplify/core";
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
