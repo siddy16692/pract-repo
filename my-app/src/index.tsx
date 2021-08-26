75592db0c82481107bd5b73224c4de289f720e84
sidnhs <siddharth.nair@nhs.net>
2021-08-26 18:01:31 +0100

    trying auth configure

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "@aws-amplify/core";
+import Auth from "@aws-amplify/auth";
 import config from './aws-exports';
-Amplify.configure(config);
+Auth.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
