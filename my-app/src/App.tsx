ca3e80fc9fe22725942b037cd299698df33838d6
sidnhs <siddharth.nair@nhs.net>
2021-08-31 21:53:12 +0100

    Adding the create,list and delete api calls to graphql in react from tutorial

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,95 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API, graphqlOperation, Storage } from 'aws-amplify';
+import { listNotes } from './graphql/queries';
+import {createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations'
+const initialFormState = {name: '', description: '', image: ''}
 
 function App() {
+  const[notes, setNotes] = useState<Object[]>([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData: any = await API.graphql(graphqlOperation(listNotes));
+    console.log(apiData);
+    const notesFromAPI:any = setNotes(apiData.data.listNotes.items);
+    if(notesFromAPI){
+    await Promise.all(notesFromAPI.map(async (note:any) => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+  }
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    console.log(formData.name);
+    if (!formData.name || !formData.description) return console.log('no');
+    const data_in:any = await API.graphql({query: createNoteMutation, variables: {input: formData}});
+    console.log(data_in);
+    if(formData.image){
+      const image:any = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote(id: string) {
+    console.log(id);
+    const newNotesArray = notes.filter((note:any) => note.id === id);
+    setNotes(newNotesArray);
+    await API.graphql({query:deleteNoteMutation, variables: {input: {id}}});
+  }
+
+  async function onChange(e: any) {
+    if (!e.target.files[0]) return;
+    const file = e.target.files[0];
+    setFormData({...formData, image: file.name});
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
   return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+        placeholder="Note name"
+        value={formData.name}></input>
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value })}
+        placeholder="Note description"
+        value={formData.description}></input>
+        <input type="file" onChange={onChange} />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map((note:any) => (
+            <div key={note.id || note.name}>
+              <h2>{note.id}</h2>
+              <h2>{note.name}</h2>
+              <h3>{note.description}</h3>
+              <button onClick={() => deleteNote(note.id)}>Delete Note</button> 
+              {
+                note.image && <img src={note.image} style={{width:400}} />
+              }
+              </div> 
+          ))
+        }
+      </div>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from "@aws-amplify/core";
-import Auth from "@aws-amplify/auth";
+
+import {API, Amplify} from "aws-amplify";
 import config from './aws-exports';
-Auth.configure(config);
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
